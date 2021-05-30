d18dc5285a18c49127d6ee777d7f9350d47e49fa
lakshma reddy <lakshma reddy@Lakshmareddy>
2021-05-30 12:18:00 +0530

    Create Project Commit

---
+++ haltungcrm/src/test/java/com/haltungcrm/action/CreateProjectTest.java
@@ -0,0 +1,14 @@
+package com.haltungcrm.action;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("CreateProjectTest Tests Running Successfully",true);
+	}
+	
+}
