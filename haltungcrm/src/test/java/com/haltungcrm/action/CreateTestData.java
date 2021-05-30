1d4c4f20acb140b6338dde9e614c18d01914aae4
lakshma reddy <lakshma reddy@Lakshmareddy>
2021-05-30 13:49:40 +0530

    Create Test Data commit

---
+++ haltungcrm/src/test/java/com/haltungcrm/action/CreateTestData.java
@@ -0,0 +1,12 @@
+package com.haltungcrm.action;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateTestData {
+	@Test
+	public void run() {
+		Reporter.log("trst running",true);
+	}
+
+}
